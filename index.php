bd01e2aa0557a44b2e0616b1d1b297a1406a8462
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-22 22:18:25 -0300

    aula_senac
    conexão com banco de dados usando php e mysql

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+    <h1>Controle de Tarefas </h1>
+    <form>
+        nome da tarefa:
+        <input name="nome">
+        finalizada?:
+        <input name="finalizada">
+        <button type="submit">Salvar</button>
+    </form>
+        <?php
+        $nome = $_GET["nome"];
+        $finalizada = $_GET["finalizada"];
+        echo $nome . " - " . $finalizada;
+
+        //dados de conexão com o banco de dados
+        $servidor = "localhost";
+        $usuario_db = "root";
+        $senha_usuario = "";
+        $banco_dados = "agenda";
+
+        //abrir a conexão com o banco de dados
+        $conexao = mysqli_connect($servidor,$usuario_db,$senha_usuario,$banco_dados);
+
+        //criar sql
+        $sql = "insert into tarefas(nome, finalizada) values('$nome', '$finalizada')";
+
+        //executar o sql no banco de dados
+        mysqli_query($conexao, $sql);
+
+        //fechar a conexão com o banco de dados
+        mysqli_close($conexao);
+        ?>
+</body>
+</html>
